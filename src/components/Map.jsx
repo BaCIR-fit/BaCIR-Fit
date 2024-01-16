43a216d8de17e9fa08caecf1a4a580708c226f06
Alexandre FB <alexandre.facon-bocquet@student.junia.com>
2024-01-16 17:13:35 +0100

    map update

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -4,22 +4,27 @@ import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 
 const Map = ({ center, zoom, markerPosition }) => {
 const mapContainerStyle = {
-width: '100%',
-height: '400px',
-margin: 'auto auto', // Ajoutez cette ligne pour centrer le conteneur
+    width: '80%',
+    height: '300px',
+    margin: 'auto auto', // Ajoutez cette ligne pour centrer le conteneur
+    
 };
+const position = [50.633333,3.066667]
 
 return (
-<MapContainer center={center} zoom={zoom} style={mapContainerStyle}>
+<MapContainer center={position} zoom={12} style={mapContainerStyle}>
+
     <TileLayer
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
     />
+    
 
     <Marker position={markerPosition}>
-    <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-    </Popup>
+        <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+
     </Marker>
 </MapContainer>
 );
