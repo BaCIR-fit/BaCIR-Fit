455e63a0753742e693b7f05d961a645d2a6a76db
Baptiste LIBERT <baptiste.libert@student.junia.com>
2024-01-16 15:07:17 +0100

    div

---
+++ src/components/DivFooter.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import logo from '../logo.svg'
+
+import "./Header.css"
+
+
+const DivHeader = () => {
+    return (
+        <div className="divHeader"></div>
+    )
+}
+
+export default DivHeader
