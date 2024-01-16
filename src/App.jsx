8fc7c18c94b1ea4c75e0b2d18d95760dcff549d2
Edouard Lambert <edouard.lambert@student.junia.com>
2024-01-16 11:30:20 +0100

    Ca va tout casser

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './App.css';
+import Accueil from './pages/Accueil';
+
+function App() {
+  return (
+    <div className="App">
+      <Accueil />
+    </div>
+  );
+}
+
+export default App;
