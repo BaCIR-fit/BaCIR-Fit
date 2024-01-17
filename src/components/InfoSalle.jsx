7089394896b2e9411842cc6f29692ad151b1169a
Marc-AntoineL <90192695+Marc-AntoineL@users.noreply.github.com>
2024-01-17 10:23:33 +0100

    Push pop Sélection Salle

---
+++ src/components/InfoSalle.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+import logo from '../CIR_fit.svg';
+import "./InfoSalle.css";
+
+
+const InfoSalle = (props) => {
+
+    let freqClass = ""; // Classe CSS par défaut
+    let freqState = "";
+
+    // Condition pour déterminer la classe CSS en fonction de la valeur de props.freq pour changer la couleur
+    if (props.freq === "Faible") {
+        freqClass = "Low";
+    } else if (props.freq === "Modérée") {
+        freqClass = "Moderate";
+    } else if (props.freq === "Elevée") {
+        freqClass = "High";
+    }
+
+    if (props.state === "Ouvert") {
+        freqState = "Low";
+    } else if (props.state === "Fermé") {
+        freqState = "High";
+    }
+    
+    return (
+        <div className="SalleData">
+            <div>
+                <img src={logo} className="header-logo" alt="logo" />
+            </div>
+            <div>
+                <div>
+                <span className='BoldViolet'>{props.title}</span>
+                </div>
+                <div>
+                    <span className='BoldViolet'>Adresse : </span>{props.adress}
+                </div>
+                <div>
+                    <span className='BoldViolet'>Horaires : </span>{props.horaires}
+                </div>
+                <div>
+                <span className='BoldViolet'>Etat : </span><span className={freqState}>{props.state}</span>
+                </div>
+                <div>
+                <span className='BoldViolet'>Fréquentation : </span><span className={freqClass}>{props.freq}</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default InfoSalle
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,17 +1,24 @@
-import 'leaflet/dist/leaflet.css'; // Importez les styles de Leaflet
-import React from 'react';
 import L from 'leaflet';
-import { MapContainer, Marker, Popup, TileLayer, HOCMarker } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css'; // Importez les styles de Leaflet
+import React, { useState } from 'react';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import InfoSalle from "../components/InfoSalle.jsx";
 import icon from '../icons/location-dot.svg';
 
 const Map = ({ center, zoom, markerPosition }) => {
 const mapContainerStyle = {
     width: '80%',
     height: '300px',
-    margin: 'auto auto', // Ajoutez cette ligne pour centrer le conteneur
-    
+    margin: 'auto auto',
 };
 
+const [infoSalleVisible, setInfoSalleVisible] = useState(false); //Afficher ou non la case des infos sur la salle
+const [selectedMarker, setSelectedMarker] = useState(null); //Afficher les bonnes datas
+
+const toggleInfoSalleVisibility = (marker) => {
+    setInfoSalleVisible(true);
+    setSelectedMarker(marker);
+};
 
 let DefaultIcon = L.icon({
     iconUrl: icon,
@@ -20,28 +27,47 @@ let DefaultIcon = L.icon({
 
 L.Marker.prototype.options.icon = DefaultIcon;
 
-const position = [50.633333,3.066667] // position lille
+  const position = [50.633333, 3.066667]; // position lille
 
-
+//Données qu'il faudra charger sur la page
+const positionsMarkers = [
+    { title: "BaCIR Fit Lille", coordinates: [50.633333, 3.066667], adress: "52 rue du Zouloulou", horaires: "8h-22h", state:"Fermé", freq:"Faible"},
+    { title: "BaCIR Fit Panam", coordinates: [48.8566, 2.3522], adress: "52 rue du Zouloulou", horaires: "8h-22h", state:"Ouvert" ,freq:"Modérée"},
+    { title: "BaCIR Fit Madrid", coordinates: [40.4168, -3.7038], adress: "52 rue du Zouloulou", horaires: "8h-22h", state:"Ouvert", freq:"Elevée",},
+];
 
 return (
-<MapContainer center={position} zoom={12} style={mapContainerStyle}>
+    <>
+    <MapContainer center={position} zoom={12} style={mapContainerStyle}>
+        <TileLayer
+        url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
+        />
 
-    <TileLayer
-    url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
-    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
-    />
-    
-    
+        {positionsMarkers.map((position, index) => (
+            <Marker
+                key={index}
+                position={position.coordinates}
+                eventHandlers={{ click: () => toggleInfoSalleVisibility(position) }}
+            >
+            <Popup>
+                {position.title}
+            </Popup>
+            </Marker>
+        ))}
 
-    <Marker position={markerPosition}>
-        
-        <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
+    </MapContainer>
 
-    </Marker>
-</MapContainer>
+    {infoSalleVisible && selectedMarker && (
+        <InfoSalle
+            title={selectedMarker.title}
+            adress={selectedMarker.adress}
+            horaires={selectedMarker.horaires}
+            state={selectedMarker.state}
+            freq={selectedMarker.freq}
+        />
+    )}
+    </>
 );
 };
 
