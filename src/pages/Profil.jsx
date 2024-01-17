f3a02bcb17184e82e91d89355102fe5928bc542a
Baptiste LIBERT <baptiste.libert@student.junia.com>
2024-01-17 14:19:08 +0100

    body

--- src/pages/Profil.jsx
+++ src/pages/Profil.jsx
@@ -3,14 +3,19 @@ import Header from "../components/Header.jsx"
 import Infos from "../components/Infos.jsx"
 import Footer from "../components/Footer.jsx"
 
+import "../components/Body.css"
+
+
 function Profil() {
     return (
+      <body>
       <div className="Profil">
         <Header />
         <Infos title="info"/>
         <Infos title="info"/>
         <Footer />
       </div>
+      </body>
     );
   }
   
