c1ef81012a0a0c4112bb90c98b658036116f24ea
Anya <anya.lallart@student.junia.com>
2024-01-18 22:16:43 +0100

    planning done

--- src/pages/Planning.jsx
+++ src/pages/Planning.jsx
@@ -6,7 +6,7 @@ import Header from "../components/Header.jsx";
 import SelectPlanning from '../components/MenuDeroulantSalles.jsx';
 
 function handleActivities(id_club){
-  fetch(`https://apibacir.fly.dev/user/getActivity/}` + id_club,{
+  fetch(`https://apibacir.fly.dev/user/getActivity/${id_club}`,{
     method:'GET',
     headers: {
       'Content-type': 'application/json; charset=UTF-8',
@@ -15,6 +15,7 @@ function handleActivities(id_club){
     .then(data => {
       try{ 
         let activityInfos = data.data;
+        console.log(activityInfos)
         localStorage.setItem("activity", JSON.stringify(activityInfos))
       } catch (err) {
         console.log(data.message)
@@ -31,7 +32,6 @@ function handleClub(){
     .then(response => response.json())
     .then(data => {
       try{ 
-        console.log("ici on envoie les infos");
         let clubsInfos = data.data;
         localStorage.setItem("club",JSON.stringify(clubsInfos))
       } catch (err) {
@@ -48,22 +48,22 @@ allClubs = JSON.parse(localStorage.getItem("club"));
 // let allActivities = [];
 // allActivities = JSON.parse(localStorage.getItem("activity"));
 const allActivities = [
-  { activity_name: 'Zumba café', start_time: '2024-01-18T10:00:00', end_time: '2024-01-18T12:00:00', room_id: 'Salle de musculation', club_name: 'BaCIR fit Lille Natio', club_id:'1' },
-  { activity_name: 'Aquaponey', start_time: '2024-01-20T14:00:00', end_time: '2024-01-20T16:00:00', room_id: 'Salle de musculation', club_name: 'BaCIR fit Lille Natio', club_id:'1' },
-  { activity_name: 'Crossfit', start_time: '2024-01-20T14:00:00', end_time: '2024-01-20T16:00:00', room_id: 'Salle de Yoga', club_name: 'BaCIR fit Lille Esquermes', club_id:'2' },
+  { activity_name: 'Zumba café', start_time: '2024-01-18T10:00:00', end_time: '2024-01-18T12:00:00', room_id: 'Salle de musculation', club_name: 'BaCIR fit Lille Natio', club_id:'65a900b351f957aebe23a237' },
+  { activity_name: 'Aquaponey', start_time: '2024-01-20T14:00:00', end_time: '2024-01-20T16:00:00', room_id: 'Salle de musculation', club_name: 'BaCIR fit Lille Natio', club_id:'65a900b351f957aebe23a237' },
+  { activity_name: 'Crossfit', start_time: '2024-01-20T14:00:00', end_time: '2024-01-20T16:00:00', room_id: 'Salle de Yoga', club_name: 'BaCIR fit Lille Esquermes', club_id:'65a9011b51f957aebe23a23c' },
 ];
 
-console.log(allActivities);
 
 function Planning() {
   const initialData = [];
   allActivities.map((activity) => {
+    console.log(activity)
     initialData.push({
       title: activity.activity_name,
       start: activity.start_time,
       end: activity.end_time,
-      salle: activity.room_id,  // Utilisez la salle (room_id) ou toute autre information pertinente
-      club: activity.club_id,   // Utilisez l'ID du club ou toute autre information pertinente
+      salle: activity.room_id,  
+      club: activity.club_name,   
       extendedProps: { isDejaInscrit: false },
     });
   });
@@ -73,8 +73,6 @@ function Planning() {
 
   const handleClubChange = (club) => {
     setSelectedClub(club);
-    console.log(club);
-    handleActivities(club);
   };
 
   useEffect(() => {
@@ -85,7 +83,7 @@ function Planning() {
     if (club === '') {
       setFilteredData(initialData);
     } else {
-      const filtered = initialData.filter((event) => event.club === club);
+      const filtered = initialData.filter((event) =>event.club === club);
       setFilteredData(filtered);
     }
   };
